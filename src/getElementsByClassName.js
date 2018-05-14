c2bd2f39c77861743494ba93e8e043bcd2ad8c6d
Deepali Garg <hdgarg@gmail.com>
2018-05-13 21:18:19 -0700

    completes successfully getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+/** 
+ * input: className as a string, node
+ * outpput: array
+ * pseudocode:
+ * define an output Array called matchingElements
+ * set node as document.body on the first run
+ *   check if node is a match
+ *     push node to matching elements
+ * concat matchingElements with the result of running getElementsBy ClassName on each child
+*/
+var getElementsByClassName = function(className, node = document.body) {
+  var matchingElements = [];
+  if (node.className && node.className.includes(className)) {
+    matchingElements.push(node);
+  }
+  node.childNodes.forEach(child => {
+    matchingElements = matchingElements.concat(getElementsByClassName(className, child));
+  });
+  return matchingElements;
 };
